2bf363cfc2135e2b3bc0531889719f7f037eefc9
Abhishek More <abhimore74@gmail.com>
2023-10-21 12:43:25 -0500

    window template

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,11 @@
 export default function Home() {
-  return <p>hello</p>;
+  return (
+    <div className="h-screen w-screen">
+      <div className="flex justify-center text-3xl min-h-[60px]">Navbar</div>
+      <div className="flex w-screen h-full gap-2 items-center">
+        <div className="w-2/5 h-full ml-4 mb-4 bg-black rounded-lg"></div>
+        <div className="w-3/5 h-full mr-4 mb-4 bg-black rounded-lg"></div>
+      </div>
+    </div>
+  );
 }
