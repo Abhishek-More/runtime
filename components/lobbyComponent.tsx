ff68bcc65786520cc9533c7f2751e85c3d025220
NitroGuy10 <naviyer10@gmail.com>
2023-10-21 23:23:24 -0500

    stuff

--- components/lobbyComponent.tsx
+++ components/lobbyComponent.tsx
@@ -10,7 +10,7 @@ export default function LobbyComponent() {
     
     return(
         <div className="px-6">
-            <div className="flex justify-between items-center h-20 px-8 my-2 rounded-lg border border-gray-100 shadow-md">
+            <div className="flex justify-between items-center h-20 px-8 my-2 rounded-lg border border-gray-100 shadow-md cursor-pointer">
                 <div className="flex items-center">
                 <img className="rounded-full h-12 w-12" src="https://static-cdn.jtvnw.net/jtv_user_pictures/27fdad08-a2c2-4e0b-8983-448c39519643-profile_image-70x70.png" alt="Logo" />
                 <div className="ml-2">
