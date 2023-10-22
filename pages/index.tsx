291e5ba0e202399e99d8c1379f678843e7e472cb
Abhishek More <abhimore74@gmail.com>
2023-10-21 23:31:42 -0500

    merged code

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,12 @@ import EditorComponent from "@/components/editorTab";
 export default function Home() {
   return (
     <div className="h-screen w-screen">
-      <div className="flex justify-center text-3xl min-h-[100px] text-sc-yellow font-extrabold font-mono pt-3 font-monda">King of the Heap</div>
+      <div className="flex justify-center text-3xl min-h-[100px] text-sc-yellow font-extrabold font-mono pt-3 font-monda">
+        King of the Heap
+      </div>
       <div className="flex w-screen h-full gap-2 items-center">
         <div className="w-2/5 h-full ml-4 mb-4 bg-sc-darkpurple rounded-lg"></div>
-        <div className="w-3/5 h-full mr-4 mb-4 bg-sc-darkpurple rounded-lg">
+        <div className="flex flex-col gap-4 w-3/5 h-full mb-4 rounded-lg">
           <EditorComponent />
         </div>
       </div>
