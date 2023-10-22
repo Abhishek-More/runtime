1fa17dde974ec04d7aea92fb9e6b7494869cdca1
Li Shen <lililogan@gmail.com>
2023-10-22 01:57:05 -0500

    ui changes on lobby

--- pages/lobby/[level].tsx
+++ pages/lobby/[level].tsx
@@ -68,18 +68,27 @@ export default function Lobby() {
     
     return(
         <div className="min-h-screen flex flex-col p-12 justify-center items-center font-monda gap-4">
-          <div className="text-5xl font-bold text-gray-700 font-metal">Enter the Ring: Find a Lobby</div>
-          <div className="flex flex-col gap-6 bg-white w-full md:max-w-2xl rounded-lg shadow border-gray-600 border-double border-2 px-6 py-8">
+            <img src="/swords.svg" alt="Swords" className="w-20 h-20"/>
+            <div className="text-5xl font-bold text-gray-700 font-metal mb-8">Enter the Ring: Find a Lobby</div>
+            <div className="flex flex-col gap-6 bg-white w-full md:max-w-2xl rounded-lg shadow border-gray-600 border-double border-2 px-6 py-8">
               <div className="flex flex-col justify-between items-center gap-6">
-                  <div className="font-bold text-gray-700">LET&apos;S GET CODING, {nickname}!</div>
-                  <div className="font-bold text-gray-700">Your Level Selection: {level}</div>
-                  {isCreatingLobby ?
-                  <p className="rounded-lg border border-1 px-12 py-4">Creating Your Lobby...</p>
-                  : <Button className="" onClick={createLobby}>CREATE PERSONAL LOBBY</Button>
-                  }
+                    <div className="flex items-center gap-2">
+                        <img src="/explosion.svg" alt="Swords" className="w-8 h-8"/>
+                        <div className="font-bold text-gray-700">LET&apos;S GET CODING, {nickname}!</div>
+                    </div>
+                    <div className="font-bold text-gray-700">Your Level Selection: {level}</div>
+                    {isCreatingLobby ?
+                    <Button className="">Creating Your Lobby...</Button>
+                    : <Button className="" onClick={createLobby}>CREATE PERSONAL LOBBY</Button>
+                    }
               </div>
               <div className="text-center">- or -</div>
-              <div className="text-center text-3xl font-bold text-gray-700">JOIN A LOBBY</div>
+              <div className="flex justify-center align-center items-center gap-2">
+                <div className="text-center text-3xl font-bold text-gray-700">JOIN A LOBBY</div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2.5" stroke="currentColor" className="w-8 h-8">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m0 0l6.75-6.75M12 19.5l-6.75-6.75" />
+                </svg>
+              </div>
               <div>
                   {lobbies.map((item) => (
                       <LobbyComponent key={item.lobby_id} lobby={item}></LobbyComponent>
